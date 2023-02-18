b277a848706fc76bef974699dfb0738c87f62f5d
SumaiyaTabassum-1520 <105984307+SumaiyaTabassum-1520@users.noreply.github.com>
2023-02-18 22:46:59 +0600

    unnecessary

--- Instructor and Course relation queries .sql
+++ Instructor and Course relation queries .sql
@@ -16,74 +16,40 @@ credits NUMBER
 );
 
 
-INSERT INTO Instructor_2021160042 	VALUES(10101,'Srinivasan','Comp.Sci.',65000);
-
-INSERT INTO Instructor_2021160042 	VALUES(12121,'Wu','Finance',90000);
-
-INSERT INTO Instructor_2021160042 	VALUES(15151,'Mozart','Music',40000);
-
-INSERT INTO Instructor_2021160042 	VALUES(22222,'Einstein','Physics',95000);
-
+INSERT INTO Instructor_2021160042
+VALUES(10101,'Srinivasan','Comp.Sci.',65000);
+INSERT INTO Instructor_2021160042
+VALUES(12121,'Wu','Finance',90000);
+INSERT INTO Instructor_2021160042
+VALUES(15151,'Mozart','Music',40000);
+INSERT INTO Instructor_2021160042
+VALUES(22222,'Einstein','Physics',95000);
 INSERT INTO Instructor_2021160042 
-	VALUES(32342,'El Said','History',60000);
-
-INSERT INTO Instructor_2021160042 	VALUES(33456,'Gold','Physics',87000);
-
+VALUES(32342,'El Said','History',60000);
+INSERT INTO Instructor_2021160042
+VALUES(33456,'Gold','Physics',87000);
 INSERT INTO Instructor_2021160042 
-	VALUES(45565,'Katz','Comp.Sci.',75000);
+VALUES(45565,'Katz','Comp.Sci.',75000);
+INSERT INTO Instructor_2021160042 VALUES(58583,'Califieri','History',62000);
+INSERT INTO Instructor_2021160042 VALUES(76543,'Singh','Finance',80000);
+INSERT INTO Instructor_2021160042 VALUES(76766,'Crick','Biology',72000);
+INSERT INTO Instructor_2021160042 VALUES(83821,'Brandt','Comp.Sci.',92000);
+INSERT INTO Instructor_2021160042 VALUES(98345,'Kim','Elec.Eng.',80000);
 
-INSERT INTO Instructor_2021160042 	VALUES(58583,'Califieri','History',62000);
-
-INSERT INTO Instructor_2021160042 	VALUES(76543,'Singh','Finance',80000);
-
-INSERT INTO Instructor_2021160042 	VALUES(76766,'Crick','Biology',72000);
-
-INSERT INTO Instructor_2021160042 	VALUES(83821,'Brandt','Comp.Sci.',92000);
-
-INSERT INTO Instructor_2021160042 	VALUES(98345,'Kim','Elec.Eng.',80000);
-
-
-
-INSERT INTO CSE302_2021160042
-	VALUES ('BIO-101','Intro.to Biology','Biology',4);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('BIO-301','Intro.to Biology','Biology',4);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('BIO-399','Genetics','Biology',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('CS-101','Intro.to Computer      
-                                       Science','Comp.Sci.',4);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('CS-190','Game Design','Comp.Sci.',4);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('CS-315','Robotics','Comp.Sci.',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('CS-319','Image Processing','Comp.Sci.',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('CS-347','Database System 
-                                       Concepts','Comp.Sci.',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('EE-181','Intro.to Digital System','Elec.Eng',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('FIN-201','Investment Banking','Finance',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('HIS-351','World History','History',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('MU-199','Music Video Production','Music',3);
-
-INSERT INTO CSE302_2021160042 
-	VALUES ('PHY-101','Physics Principles','Physics',4);
+INSERT INTO CSE302_2021160042 VALUES ('BIO-101','Intro.to Biology','Biology',4);
+INSERT INTO CSE302_2021160042 VALUES ('BIO-301','Intro.to Biology','Biology',4);
+INSERT INTO CSE302_2021160042 VALUES
+('BIO-399','Genetics','Biology',3);
+INSERT INTO CSE302_2021160042 VALUES ('CS-101','Intro.to Computer Science','Comp.Sci.',4);
+INSERT INTO CSE302_2021160042 VALUES ('CS-190','Game Design','Comp.Sci.',4);
+INSERT INTO CSE302_2021160042 VALUES ('CS-315','Robotics','Comp.Sci.',3);
+INSERT INTO CSE302_2021160042 VALUES ('CS-319','Image Processing','Comp.Sci.',3);
+INSERT INTO CSE302_2021160042 VALUES ('CS-347','Database System Concepts','Comp.Sci.',3);
+INSERT INTO CSE302_2021160042 VALUES ('EE-181','Intro.to Digital System','Elec.Eng',3);
+INSERT INTO CSE302_2021160042 VALUES ('FIN-201','Investment Banking','Finance',3);
+INSERT INTO CSE302_2021160042 VALUES ('HIS-351','World History','History',3);
+INSERT INTO CSE302_2021160042 VALUES ('MU-199','Music Video Production','Music',3);
+INSERT INTO CSE302_2021160042 VALUES ('PHY-101','Physics Principles','Physics',4);
 
 
 
